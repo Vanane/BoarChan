4d56925fd470134472e881353800ec984a6cfae3
Vanane <alvin.auvray@gmx.fr>
2021-01-18 23:37:29 +0100

    ajout viewthread

--- src/newthread.php
+++ src/newthread.php
@@ -1,4 +1,20 @@
+<?php
+    if(!isset($_GET["name"]))
+    {
+?>
+            
 <form action="new.php" method="GET">
-    Title : <input type="text" placeholder="title"/>
+    Title : <input type="text" name="name" placeholder="title"/>
     <input type="submit" value="Create"/>
 </form>
+
+<?php
+    }
+    else
+    {
+        pg_query($conn, "INSERT INTO thread (title) VALUES(".$_GET["name"].");");
+        $result = pg_query($conn, "SELECT * FROM thread WHERE id=MAX(id) LIMIT 1;");
+        $row = pg_fetch_row($result);
+        echo "<a href='thread/".$row[0]."'>thread created !</a>";
+    }
+?>
--- src/viewthread.php
+++ src/viewthread.php
@@ -1,5 +1,16 @@
 <?php
 
-    echo "\n\nthread number : ".$path[2]."\n\n"; 
+    echo "\n\nthread number : ".$path[2]."\n\n";
+    $result = pg_query($conn, "SELECT * FROM message WHERE id=".$path[2].";");
+    if(!$result)
+        echo "This thread doesn't exist...Yet !";
+    else
+    {
+        while ($row = pg_fetch_row($result))
+        {
+            echo "<p>Message N°".$row[0]."</p>";
+            echo "<p>".$row[1]."</p>";
+        }
+    }
 
 ?>
