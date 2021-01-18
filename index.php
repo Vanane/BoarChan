80d965f8b015a5c5b347f7cf68d072fc84bc50c9
Vanane <alvin.auvray@gmx.fr>
2021-01-18 20:50:29 +0100

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" href="src/css.css">
+        <title>BoarChan - Like 4Chan, but not the same.</title>
+    </head>
+    <body>
+
+
+<?php
+    include("src/menu.php");
+?>
+
+
+    </body>
+</html>
