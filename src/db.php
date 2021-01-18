ceb8126005bc2f295905939b3931b023a7f1cd10
Vanane <alvin.auvray@gmx.fr>
2021-01-18 23:15:03 +0100

    sql

--- src/db.php
+++ src/db.php
@@ -1,7 +1,6 @@
 <?php
-include("libpq");
-
 $conn = pg_connect(getenv("DATABASE_URL"));
 
-pg_query($conn, "create table message(id SERIAL content VARCHAR(512), thread INT);");
+pg_query($conn, "CREATE TABLE thread(id SERIAL, title VARCHAR(128));");
+pg_query($conn, "CREATE TABLE message(id SERIAL, content VARCHAR(512), threadid INT, CONSTRAINT fk_message_thread FOREIGN KEY (threadid) REFERENCES thread(id));");
 ?>
