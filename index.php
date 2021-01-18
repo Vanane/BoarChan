df50825411592e39420f30c3af07c0291a3a49f1
Vanane <alvin.auvray@gmx.fr>
2021-01-18 22:52:35 +0100

    correction relpath

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link rel="stylesheet" href="/app/src/css.css">
+        <link rel="stylesheet" href="/src/css.css">
         <title>BoarChan - Like 4Chan, but not the same.</title>
     </head>
     <body>
