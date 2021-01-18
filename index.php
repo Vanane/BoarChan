65454d674f594cf3043bcc0f4f4e53e0627720bb
Vanane <alvin.auvray@gmx.fr>
2021-01-18 22:05:24 +0100

    correction index

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="/src/css.css">
+        <link rel="stylesheet" href="src/css.css">
         <title>BoarChan - Like 4Chan, but not the same.</title>
     </head>
     <body>
 
 
 <?php
-    include("/src/menu.php");
+    include("src/menu.php");
 ?>
 
 
