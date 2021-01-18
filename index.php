4cd9790df161f3f033c267c6860d039bb76210ca
Vanane <alvin.auvray@gmx.fr>
2021-01-18 22:24:14 +0100

    correction relpath css

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="src/css.css">
+        <link rel="stylesheet" href="/src/css.css">
         <title>BoarChan - Like 4Chan, but not the same.</title>
     </head>
     <body>
-
-
-<?php
-    include("src/menu.php");
-?>
-
-
+    <?php
+        include("src/menu.php");
+    ?>
     </body>
 </html>
