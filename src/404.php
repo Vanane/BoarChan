282723097e46ea1432fe79b095ec5b46067a1d34
Vanane <alvin.auvray@gmx.fr>
2021-01-19 22:10:01 +0100

    ajout 404 propre

--- src/404.php
+++ src/404.php
@@ -1,9 +1,10 @@
-<html>
-    <body>
-    hehe boi
-    <?php
-    echo $_SERVER["REQUEST_URI"];
-    var_dump($_GET);
-    ?>
-    </body>
-</html>
+Page not found.
+<?php
+$jokes = array("You weren't trying to make shadowy URLs, were you ?", "But you know what you've found ? Love.", "Or is it ? Hey VSauce, Michael here.");
+$rand = rand(0, count($jokes) - 1);
+
+$picked = $jokes[$rand];
+echo " $picked";
+
+
+?>
