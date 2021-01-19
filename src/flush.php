c0eab79571be01e065eb3598676109b85f5cd11b
Vanane <alvin.auvray@gmx.fr>
2021-01-19 22:25:52 +0100

    ajout timestamp

--- src/flush.php
+++ src/flush.php
@@ -1,6 +1,6 @@
 <?php
 pg_query($conn, "DROP TABLE IF EXISTS message;");
 pg_query($conn, "DROP TABLE IF EXISTS thread;");
-pg_query($conn, "CREATE TABLE thread(id SERIAL PRIMARY KEY, title VARCHAR(128));");
-pg_query($conn, "CREATE TABLE message(id SERIAL PRIMARY KEY, content VARCHAR(512), threadid INT, CONSTRAINT fk_message_thread FOREIGN KEY (threadid) REFERENCES thread(id));");
+pg_query($conn, "CREATE TABLE thread(id SERIAL PRIMARY KEY, title VARCHAR(128), lastMessage INT);");
+pg_query($conn, "CREATE TABLE message(id SERIAL PRIMARY KEY, content VARCHAR(512), threadid INT, stamp TIMESTAMP, CONSTRAINT fk_message_thread FOREIGN KEY (threadid) REFERENCES thread(id));");
 ?>
--- src/send.php
+++ src/send.php
@@ -3,7 +3,13 @@
     {
         $thread = $_POST["thread"];
         $content = $_POST["content"];
-        pg_query($conn, "INSERT INTO message(content, threadid) VALUES ('$content', '$thread');");
+        $curtime = date('Y-m-d H:i:s');
+        if($content != "")
+        {
+            $result = pg_query($conn, "INSERT INTO message(content, threadid, stamp) VALUES ('$content', '$thread', '$curtime') RETURNING id;");
+            $messageNum = pg_fetch_row($result)[0];
+            pg_query($conn, "UPDATE thread SET lastMessage=$messageNum WHERE id=$thread;");
+        }
     }
     $host = $_SERVER['HTTP_HOST'];
     $thread = isset($_POST["thread"]) ? "/".$_POST["thread"] : "";
