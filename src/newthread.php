035636d8e648fa6c08f487f37211564f1d6cb53a
Vanane <alvin.auvray@gmx.fr>
2021-01-19 21:54:26 +0100

    ajout d'un message quand créer thread

--- src/newthread.php
+++ src/newthread.php
@@ -1,10 +1,11 @@
 <?php
-    if(!isset($_POST["name"]))
+    if(!isset($_POST["name"]) || !isset($_POST["messag"]))
     {
 ?>
             
 <form action="new" method="POST">
     Title : <input type="text" name="name" placeholder="title"/>
+    First message : <input type="textarea" name="message" placeholder="message"/>
     <input type="submit" value="Create"/>
 </form>
 
@@ -14,6 +15,7 @@
     {
         $result = pg_query($conn, "INSERT INTO thread (title) VALUES('".$_POST["name"]."') RETURNING id;");
         $row = pg_fetch_row($result);
+        pg_query($conn, "INSERT INTO message (content, threadid) VALUES('".$_POST["message"]."', '$row[0]');");
         echo "<a href='thread/".$row[0]."'>thread created !</a>";
     }
 ?>
